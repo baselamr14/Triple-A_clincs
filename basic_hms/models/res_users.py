d4a896d9b31a717d5a72533cb506f0e1c6dfce86
Bassel Amr <bamr65@gmail.com>
2026-04-05 19:00:27 +0200

    Add clinic permissions section on users

---
+++ basic_hms/models/res_users.py
@@ -0,0 +1,59 @@
+# basic_hms/models/res_users.py
+from odoo import api, fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    clinic_doctor = fields.Boolean(string="Doctor")
+    clinic_chemist = fields.Boolean(string="Chemist")
+    clinic_medical_group = fields.Boolean(string="Medical Group")
+    clinic_receptionist = fields.Boolean(string="Receptionist")
+
+    def _apply_clinic_groups(self):
+        doctor_group = self.env.ref('basic_hms.bi_group_doctor', raise_if_not_found=False)
+        chemist_group = self.env.ref('basic_hms.group_chemist', raise_if_not_found=False)
+        medical_group = self.env.ref('basic_hms.group_medical_group', raise_if_not_found=False)
+        receptionist_group = self.env.ref('basic_hms.group_receptionist', raise_if_not_found=False)
+
+        for user in self:
+            if doctor_group:
+                if user.clinic_doctor:
+                    user.groups_id = [(4, doctor_group.id)]
+                else:
+                    user.groups_id = [(3, doctor_group.id)]
+
+            if chemist_group:
+                if user.clinic_chemist:
+                    user.groups_id = [(4, chemist_group.id)]
+                else:
+                    user.groups_id = [(3, chemist_group.id)]
+
+            if medical_group:
+                if user.clinic_medical_group:
+                    user.groups_id = [(4, medical_group.id)]
+                else:
+                    user.groups_id = [(3, medical_group.id)]
+
+            if receptionist_group:
+                if user.clinic_receptionist:
+                    user.groups_id = [(4, receptionist_group.id)]
+                else:
+                    user.groups_id = [(3, receptionist_group.id)]
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        users = super().create(vals_list)
+        users._apply_clinic_groups()
+        return users
+
+    def write(self, vals):
+        res = super().write(vals)
+        if any(field in vals for field in [
+            'clinic_doctor',
+            'clinic_chemist',
+            'clinic_medical_group',
+            'clinic_receptionist',
+        ]):
+            self._apply_clinic_groups()
+        return res
